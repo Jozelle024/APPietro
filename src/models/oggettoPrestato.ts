0076be4ffcf909fc5d2848ef4faa9bcb3d837a0d
Jozelle <jozellelauzon024@gmail.com>
2018-03-20 10:11:35 +0100

    creato classi oggettoPrestato e user

---
+++ src/models/oggettoPrestato.ts
@@ -0,0 +1,6 @@
+export class OggettoPrestato {
+    id: number
+    nome: string
+    stato: boolean // se è stato restituito
+    idUser: number // id dello user a cui è stato prestato l'oggetto
+}
